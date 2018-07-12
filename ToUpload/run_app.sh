78fd3a7bf1a5581628a9dfd908e1170d7fa3c76c
mirr254 <sammysteppa90@gmail.com>
2018-07-12 11:41:14 +0300

    setup provisioner using playbook

--- ToUpload/run_app.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-#cd to where the app is downloaded
-
-cd /var/www/brigt-events
-#activate virtual environment
-source venv-api/bin/activate
-
-#start app using gunicorn
-sudo gunicorn run:app
